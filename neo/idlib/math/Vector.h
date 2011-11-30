b3ab958e39cbd3b4965d6c8ebed0f7840cdb068b
kholdfuzion <kholdfuzion@gmail.com>
2011-11-30 01:21:12 -0600

    Fix missing header

--- neo/idlib/math/Vector.h
+++ neo/idlib/math/Vector.h
@@ -29,6 +29,7 @@ If you have questions concerning this license or the applicable additional terms
 #ifndef __MATH_VECTOR_H__
 #define __MATH_VECTOR_H__
 
+#include <cstddef>
 /*
 ===============================================================================
 
